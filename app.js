93c87843b6e576b98d18eda03c981f6839f27ada
rohan-joby <rohanjobyak@gmail.com>
2021-04-24 15:17:39 +0530

    wip: add input,processing,output part
    Form required url endpoint using user input .
    Create function to handle Click event which will make fetch request on user click

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const outputText = document.querySelector(".output__text");
 
 const urlServer = "https://api.funtranslations.com/translate/shakespeare.json";
 
-function getUrl(text){
-    console.log(urlServer + "?text=" + text);
+function getUrl(text) {
+  return urlServer + "?text=" + text;
 }
 
+function handleClick() {
+  //getting input
+  const input = inputText.value;
 
+  //processinng input
+  fetch(getUrl(input))
+    .then((res) => res.json)
+    .then((json) => {
+      const translatedText = json;
+      /*outputText.innerText*/ console.log(translatedText); //display output
+    });
+}
 
-button.addEventListener("click", () => getUrl(inputText.value));
+button.addEventListener("click", handleClick);
